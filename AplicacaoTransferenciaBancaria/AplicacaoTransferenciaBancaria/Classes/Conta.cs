bb1ab9fea8e29605de6b298a108c659f04698fc0
RafaelBonotto <rafareisaba@gmail.com>
2021-11-06 00:39:23 -0300

    Métodos da classe Conta e Menu do usuário na classe Program

--- AplicacaoTransferenciaBancaria/AplicacaoTransferenciaBancaria/Classes/Conta.cs
+++ AplicacaoTransferenciaBancaria/AplicacaoTransferenciaBancaria/Classes/Conta.cs
@@ -16,5 +16,41 @@ namespace AplicacaoTransferenciaBancaria.Classes
             this.Credito = credito;
             this.Nome = nome;               
         }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito *- 1))
+            {
+                System.Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            System.Console.WriteLine($"Saldo atual da conta de {this.Nome} é {this.Saldo}");
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            System.Console.WriteLine($"Saldo atual da conta de {this.Nome} é {this.Saldo}");
+        }
+
+        public void Transferir(double valorTransferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTransferencia))
+            {
+                contaDestino.Depositar(valorTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += $"TipoConta {this.TipoConta} |";
+            retorno += $"Nome {this.Nome} |";
+            retorno += $"Saldo {this.Saldo} |";
+            retorno += $"Crédito {this.Credito}";
+            return retorno;
+        }
     }
 }
--- AplicacaoTransferenciaBancaria/AplicacaoTransferenciaBancaria/Program.cs
+++ AplicacaoTransferenciaBancaria/AplicacaoTransferenciaBancaria/Program.cs
@@ -1,12 +1,112 @@
-﻿using System;
+﻿using AplicacaoTransferenciaBancaria.Classes;
+using AplicacaoTransferenciaBancaria.Enum;
+using System;
+using System.Collections.Generic;
 
 namespace AplicacaoTransferenciaBancaria
 {
     class Program
     {
+        static List<Conta> ListContas = new List<Conta>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcaoUsuario = ObterOpcaoDoUsuario();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                        //Transferir();
+                        break;
+                    case "4":
+                        //Sacar();
+                        break;
+                    case "5":
+                        //Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcaoUsuario = ObterOpcaoDoUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+       
+        private static string ObterOpcaoDoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank ao sue dispor!!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar tela");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova conta");
+
+            Console.Write("Digite 1 para conta Fisica ou 2 para conta Juridica: ");
+            int entradaTipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o nome do cliente: ");
+            string entradaNome = Console.ReadLine();
+
+            Console.Write("Digite o saldo inicial: ");
+            double entradaSaldo = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o crédito: ");
+            double entradaCredito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)entradaTipoConta,
+                                        saldo: entradaSaldo,
+                                        credito: entradaCredito,
+                                        nome: entradaNome);
+
+            ListContas.Add(novaConta);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar Contas");
+
+            if (ListContas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma conta cadastrada");
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < ListContas.Count; i++)
+                {
+                    Conta conta = ListContas[i];
+                    Console.Write($"#{i} - ");
+                    Console.WriteLine(conta);
+                }
+            }
         }
     }
 }
